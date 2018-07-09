23696d247becc11120afa40855019c1ba71ac467
seul lee <blacksangi@gmail.com>
2018-07-09 21:08:53 +0900

    requests_html 사용

--- crawler.py
+++ crawler.py
@@ -1,45 +1,47 @@
 # coding: utf-8
 
 
-import requests
-from bs4 import BeautifulSoup
+from requests_html import HTMLSession
 
 
-def get_html(url):
-    response = requests.get(url)
-    if not response.status_code == 200:
-        print(response.status_code)
-        return 
-
-    return response.text
-
-
-def parsing_book_info(html_data):
+def get_book_info(r):
     data = {}
+    base = r.html.find('.cite_info')[0]
 
-    soup = BeautifulSoup(html_data, 'html.parser')
-    base = '#content > div.cite_area_wrap > div.cite_area > div '
+    data['title'] = base.find('strong')[0].text
+    data['publisher'] = base.find('.sub_info')[2].text 
 
-    data['title'] = soup.select(base + '> strong')[0].text
-    data['publisher'] = soup.select(base + '> p > span')[1].text
-    
     return data
 
 
-if __name__ == "__main__":
+def get_concept_list(r, subject='수학'):
+    data = {}
+    base = r.html.find('.contents_sub')
 
-    # 초등수학 개념사전
-    #url = 'https://terms.naver.com/list.nhn?cid=42426&categoryId=42426'
-
-    # 학습용어 개념사전 
-    url = 'https://terms.naver.com/list.nhn?cid=43672&categoryId=43672&so=st4.asc' 
-
-    html = get_html(url)
-    if html:
-        book_info = parsing_book_info(html)
-        print(book_info)
-
+    for idx, sub in enumerate(base):
+        temp = sub.find('.contents_item')[0].text
+        raw_subject = temp.split()[0]
+        if raw_subject == subject or not subject:
+            raw_list = sub.find('ul')
+            concept_list = raw_list[0].text
+            data[raw_subject] = concept_list.split('\n')
         
+    return data
+
+
+
+
+if __name__ == '__main__':
+    url = 'https://terms.naver.com/list.nhn?cid=43672&categoryId=43672&so=st4.asc'
+
+    session = HTMLSession()
+    response = session.get(url)
+
+    book_info = get_book_info(response)
+    concept_list = get_concept_list(response)
+
+    print(book_info)
+    print(concept_list)
 
 
 
