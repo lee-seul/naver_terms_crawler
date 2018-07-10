4ba499531aed056d890e647a0cd05b6055ad173a
seul lee <blacksangi@gmail.com>
2018-07-10 12:10:48 +0900

    3 대응 및 add gitignore

--- crawler_3.py
+++ crawler_3.py
@@ -6,11 +6,32 @@ from crawler_main import get_book_info
 
 
 def get_concept_list_3(r):
-    data = {}
-    base = r.html.find()
+    result = []
+    subjects = r.html.find('div.subject')[1:]
     
+    for sub in subjects:
+        concept = {}
+        concept['name'] = sub.text
+        concept['url'] = list(sub.absolute_links)[0]
 
-    return data
+        result.append(concept)
+    return result
+
+
+def request_concept_list_per_page():
+    data = {}
+    session = HTMLSession()
+
+    data['수학'] = []
+    for i in range(1, 16):
+        url = f'https://terms.naver.com/list.nhn?cid=42427&page={i}&categoryId=42427'
+        response = session.get(url)
+        
+        concept_list = get_concept_list_3(response)
+        data['수학'].append(concept_list)
+
+    return data    
+
 
 
 if __name__ == '__main__':
@@ -20,7 +41,7 @@ if __name__ == '__main__':
     response = session.get(url) 
 
     book_info = get_book_info(response)
-    concept_list = get_concept_list_3(response)
+    concept_list = request_concept_list_per_page()
 
     print(book_info)
     print(concept_list)
